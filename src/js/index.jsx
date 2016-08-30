575b6ee382b7ab68d93f688a985d1cfca25419a5
Aaron Pflaumer <teahermit@gmail.com>
2016-08-30 13:50:10 -0700

    Gitignore, dependencies, react test.

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,2 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import Layout from './components/Layout/Layout.jsx';
+
 require('./components/common/colors.scss');
 require('./components/common/constants.scss');
+
+const app = document.getElementById('app');
+ReactDOM.render(
+  <div>
+    // <Layout />
+    Test
+  </div>,
+app);
